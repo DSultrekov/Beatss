d6a4ac43b84ac92385cc9ca9e87ee0a2c37cdcde
Данил <88366457+DSultrekov@users.noreply.github.com>
2021-10-12 22:47:03 +0700

    Add files via upload

--- js/fullscreenmenu.js
+++ js/fullscreenmenu.js
@@ -1,9 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.fullscreen__menu')
+const menu = document.querySelector('.fullscreen__menu');
+const cross = document.querySelector('.cross_icon')
 
-function tooglepage(){
-  hamburger.classList.toggle('hamburger-active');
-  menu.classList.toogle('hamburger-active');
+function tooglepage(e){
+  e.preventDefault()
+  menu.classList.add('fullscreen__menu-active');
+}
+
+function tooglecross(e) {
+  e.preventDefault()
+  menu.classList.remove('fullscreen__menu-active')
 }
 
 hamburger.addEventListener('click' , tooglepage);
+cross.addEventListener('click' , tooglecross);
