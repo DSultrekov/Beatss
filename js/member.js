f361907ad6625d5ee23dcea583a5c5320a3bd090
Данил <88366457+DSultrekov@users.noreply.github.com>
2021-10-17 22:41:59 +0700

    Add files via upload

--- js/member.js
+++ js/member.js
@@ -6,6 +6,7 @@ item.addEventListener('click', function(event){
   const parentEL = event.target.parentElement.parentElement;
   const memberDescription = parentEL.querySelector('.member__description');
   const arrow = parentEL.querySelector('.member__name-arrow');
+  const openEL = document.querySelectorAll('.member__description_visible')
 
   if (arrow.classList.contains('member__name-arrow_rotated')){
     arrow.classList.remove('member__name-arrow_rotated');
@@ -13,10 +14,15 @@ item.addEventListener('click', function(event){
     arrow.classList.add('member__name-arrow_rotated');
   }
 
+ for(const i=0; i<openEL.length;i++){
+  openEL[i].classlist.remove('member__description_visible');
+ }
+
   if (memberDescription.classList.contains('member__description_visible')){
     memberDescription.classList.remove('member__description_visible');
   } else {
     memberDescription.classList.add('member__description_visible');
   }
+  
 })
 })
---
+++ js/tabs.js
@@ -0,0 +1,8 @@
+$(".reviews__switcher-link").click(e => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const curItem = $this.closest(".reviews__switcher-item");
+
+  curItem.addClass("active").siblings().removeClass("active");
+});
